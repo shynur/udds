6ac3352482d05a59567d6adc653d819c64cc9287
谢骐 <shynur@outlook.com>
2025-06-10 18:17:55 +0800

    ;

--- contrib/make-in-rbk-v3.4.8-build-kit.bash
+++ contrib/make-in-rbk-v3.4.8-build-kit.bash
@@ -12,3 +12,11 @@ CC='clang-20 --gcc-install-dir=/usr/local/lib/gcc/x86_64-pc-linux-gnu/15.1.0'  \
 CXX='clang++-20 --gcc-install-dir=/usr/local/lib/gcc/x86_64-pc-linux-gnu/15.1.0'  \
 CFLAGS+=' -g3' CXXFLAGS+=' -g3'  \
 make
+
+make install
+
+mkdir -p bin
+clang++-6.0 -std=c++14 -O0 -g3 -I./include  \
+    -static-libgcc -static-libstdc++  \
+    src/easily-send-receive.cpp  \
+    bin/easily-send-receive
