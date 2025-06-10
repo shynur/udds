f4f9e12c0a58b7dec87f63e05ead7cc3357ed0ef
谢骐 <shynur@outlook.com>
2025-06-10 15:20:47 +0800

    ;

--- contrib/make-in-rbk-v3.4.8-build-kit.bash
+++ contrib/make-in-rbk-v3.4.8-build-kit.bash
@@ -10,4 +10,5 @@ clear
 
 CC='clang-20 --gcc-install-dir=/usr/local/lib/gcc/x86_64-pc-linux-gnu/15.1.0'  \
 CXX='clang++-20 --gcc-install-dir=/usr/local/lib/gcc/x86_64-pc-linux-gnu/15.1.0'  \
+CFLAGS+=' -g3' CXXFLAGS+=' -g3'  \
 make
