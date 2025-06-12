f0348cccac3e62bb268e0be67bad6883e1c0db83
谢骐 <shynur@outlook.com>
2025-06-12 20:15:14 +0800

    ;

--- contrib/pack-for-rbk.bash
+++ contrib/pack-for-rbk.bash
@@ -4,24 +4,26 @@ if ! [ $USER = shynur ]; then
     exit 1
 fi
 
-cd /tmp
-rm -rf udds
-git clone git@github.com:shynur/udds.git
-
-cd udds
-rm -rf .git
-git clone https://cnb.cool/seer-robotics/robokit/rbk3/sdk/utils/udds.git
-mv udds/.git .
-rm -r udds
-
 (
-    cd include/udds
-    mv udds-broadcast-client.hpp{,.bak}
-    echo '#define SHYNUR_UDDS_USED_BY_SEER_RBK 30408UL' > udds-broadcast-client.hpp
-    cat udds-broadcast-client.hpp.bak >> udds-broadcast-client.hpp
-    rm udds-broadcast-client.hpp.bak
+    cd /tmp
+    rm -rf udds
+    git clone git@github.com:shynur/udds.git
+
+    cd udds
+    rm -rf .git
+    git clone https://cnb.cool/seer-robotics/robokit/rbk3/sdk/utils/udds.git
+    mv udds/.git .
+    rm -r udds
+
+    (
+        cd include/udds
+        mv udds-broadcast-client.hpp{,.bak}
+        echo '#define SHYNUR_UDDS_USED_BY_SEER_RBK 30408UL' > udds-broadcast-client.hpp
+        cat udds-broadcast-client.hpp.bak >> udds-broadcast-client.hpp
+        rm udds-broadcast-client.hpp.bak
+    )
+    git add . && git commit -m ';' && git push
 )
-git add . && git commit -m ';' && git push
 
 cd `dirname $0`/../bin
 if [ -f udds-broadcast-cli.d/udds-broadcast-cli ]; then
