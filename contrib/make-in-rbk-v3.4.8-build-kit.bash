cbb76e5e61d669de88cb16754f5aaa3f34d2cc44
谢骐 <shynur@outlook.com>
2025-06-30 11:53:29 +0800

    ;

--- contrib/make-in-rbk-v3.4.8-build-kit.bash
+++ contrib/make-in-rbk-v3.4.8-build-kit.bash
@@ -18,6 +18,7 @@ echo '开始编译 bin/easily-send-receive...'
 clang++-6.0 -std=c++14 -Wno-c++17-extensions  \
     -O0 -g3 -ggdb -glldb -I./include  \
     -static-libgcc -static-libstdc++  \
+    -D'SHYNUR_UDDS_USED_BY_SEER_RBK=30408UL'  \
     src/easily-send-receive.cpp  \
     -o bin/easily-send-receive
 echo '编译完成: bin/easily-send-receive'
