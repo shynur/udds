125d701aac1bd4036ebb083e04eff02b05a5034f
shynur <shynur@outlook.com>
2025-11-19 14:52:54 +0800

    ;;

--- src.rpc/a.cpp
+++ src.rpc/a.cpp
@@ -8,22 +8,26 @@ struct Server: ::shynur::udds_rpc::Application::ServerImpl {
         const auto result = x + y;
         return result;
     }
-    auto subtraction(const ::ShynurUrpcProcessorServer_ClientContext&,
-        std::int32_t x, std::int32_t y
-    ) -> std::int32_t override {
-        const auto result = x - y;
+    auto floor(const ::ShynurUrpcProcessorServer_ClientContext&,
+        const UddsTwoDouble& args
+    ) -> UddsTwoInt override {
+        UddsTwoInt result;
+        result.x(args.x());
+        result.y(args.y());
         return result;
     }
 };
-struct Addition: ::shynur::udds_rpc::ClientApp::Operation {
-    Addition(const std::int32_t x, const std::int32_t y): x{x}, y{y} {}
+struct AdditionClient: ::shynur::udds_rpc::ClientApp::Operation {
     const std::int32_t x, y;
+    AdditionClient(const std::int32_t x, const std::int32_t y): x{x}, y{y} {}
+
     auto execute() -> OperationStatus override {
         std::int32_t result;
         const auto op_status = this->call_rpc(
             &::ShynurUrpcProcessor::addition, result, this->x, this->y
         );
 
+        assert(op_status == SUCCESS);
         ::shynur::utils::Logger{"INFO"}
             << "ClientApp"
             << "Addition result == " << result << "!!!";
@@ -31,6 +35,29 @@ struct Addition: ::shynur::udds_rpc::ClientApp::Operation {
         return op_status;
     }
 };
+struct FloorClient: ::shynur::udds_rpc::ClientApp::Operation {
+    const double x, y;
+    FloorClient(const double x, const double y): x{x}, y{y} {}
+
+    auto execute() -> OperationStatus override {
+        UddsTwoInt result;
+        UddsTwoDouble arg;
+        arg.x(this->x), arg.y(this->y);
+
+        const auto op_status = this->call_rpc(
+            &::ShynurUrpcProcessor::floor,
+            result, arg
+        );
+
+        assert(op_status == SUCCESS);
+        ::shynur::utils::Logger{"INFO"}
+            << "ClientApp"
+            << "Floor result == " << result.x() << result.y() << "!!!";
+
+        return op_status;
+    }
+};
+
 int main(const int argc, const char *const argv[]) {
     auto options = ::shynur::udds_rpc::Application::Options{};
     for (auto i = 1; i != argc; i++)
@@ -42,13 +69,15 @@ int main(const int argc, const char *const argv[]) {
             options.thread_pool_size = std::stoul(arg.substr(19));
 
     auto app = ::shynur::udds_rpc::Application::make_app<Server>(options);
-    if (options.entity == "client")
-        for (auto i = 0; i != 3; i++) {
-            std::int32_t x, y;
-            std::cin >> x >> y;
-            std::dynamic_pointer_cast<::shynur::udds_rpc::ClientApp>(app)->call(::Addition{x, y});
-        }
-    else {
+    if (options.entity == "client") {
+        std::int32_t x, y;
+        std::cin >> x >> y;
+        std::dynamic_pointer_cast<::shynur::udds_rpc::ClientApp>(app)->call(AdditionClient{x, y});
+
+        double p, q;
+        std::cin >> p >> q;
+        std::dynamic_pointer_cast<::shynur::udds_rpc::ClientApp>(app)->call(FloorClient{p, q});
+    } else {
         const auto _ = std::jthread{&::shynur::udds_rpc::Application::run, app};
         static std::function<void()> stop_app_handler = [=] {app->stop();};
         signal(SIGINT, +[](int) {stop_app_handler();});
