5622b830837995148721c41cdc305222d6e3abad
shynur <shynur@outlook.com>
2025-12-02 22:30:05 +0800

    .

--- src.rpc/urpc.hpp
+++ src.rpc/urpc.hpp
@@ -10,7 +10,7 @@ using namespace std::literals;
 namespace shynur::utils {
     struct [[gnu::weak]] Logger {
         static inline std::atomic_bool enabled = std::string{
-            std::getenv("URPC_LOG") or ""
+            std::getenv("URPC_LOG") ? std::getenv("URPC_LOG") : ""
         }.length() >= 1;
         const std::string_view                                   level;
         const std::chrono::time_point<std::chrono::system_clock> now;
